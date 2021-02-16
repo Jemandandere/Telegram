66b32dd1ff6bc8376486f5c52567e28ceb482997
Jemand Andere <jemandandere@gmail.com>
2021-02-16 16:12:30 +0300

    add drawer

--- app/src/main/java/com/jemandandere/telegram/MainActivity.kt
+++ app/src/main/java/com/jemandandere/telegram/MainActivity.kt
@@ -2,10 +2,63 @@ package com.jemandandere.telegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.jemandandere.telegram.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawer: Drawer
+    private lateinit var header: AccountHeader
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
     }
+
+    private fun init() {
+        toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createHeader() {
+        header = AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        ProfileDrawerItem().withName(getString(R.string.default_username))
+                                .withEmail(getString(R.string.default_phone_number))
+                )
+                .build()
+    }
+
+    private fun createDrawer() {
+        drawer = DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withSelectedItem(-1)
+                .withAccountHeader(header)
+                .addDrawerItems(
+                        PrimaryDrawerItem()
+                                .withIdentifier(100)
+                                .withIconTintingEnabled(true)
+                                .withName(getString(R.string.group_create))
+                                .withSelectable(false)
+                )
+                .build()
+    }
+
 }
