7c9be0d82452c115d3272b500dea4b40e042d9ca
Jemand Andere <jemandandere@gmail.com>
2021-02-24 20:28:35 +0300

    add settings menu

--- app/src/main/java/com/jemandandere/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/jemandandere/telegram/ui/fragments/SettingsFragment.kt
@@ -1,8 +1,18 @@
 package com.jemandandere.telegram.ui.fragments
 
+import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.jemandandere.telegram.R
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.settings_action_menu, menu)
+    }
 }
